8c8e082cf8ebf3d684ee63d68f0d0bb110b3d65a
Raghu Betina <raghu@firstdraft.com>
2019-06-03 21:12:49 +0000

    Direct association between restaurants and items

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :items,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Restaurant, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:items) }
+
     end
 
     describe "InDirect Associations" do
