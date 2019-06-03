1e411157e37236786e327b3efedc642f51112604
Raghu Betina <raghu@firstdraft.com>
2019-06-03 21:12:50 +0000

    Direct association between items and reviews

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :dependent => :destroy
+
   belongs_to :restaurant
 
   # Indirect associations
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Item, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:reviews) }
+
     it { should belong_to(:restaurant) }
 
     end
